36ba36403185ac956ddb8fc47c27282ee2fb6fd3
nimesh-govind <nimeshgovind74@gmail.com>
2020-07-14 21:58:19 +1200

    game challenge complete

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -22,9 +24,15 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue(evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
-
+function hide(evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 function updateCounts () {
   var totals = {
     blue: 0,
@@ -33,7 +41,14 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+var blueCount = document.getElementsByClassName('blue').length;
+totals.blue = blueCount
 
+var greenCount = document.getElementsByClassName('green').length;
+totals.green = greenCount
+
+var invisibleCount = document.getElementsByClassName('invisible').length;
+totals.invisible = invisibleCount
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
